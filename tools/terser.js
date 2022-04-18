1cfdbf11a35797949079df8a46cd5daa68d6d2c5
RuntimeOverflow <mail@runtimeoverflow.com>
2022-04-18 15:35:22 +0200

    Formatting

--- tools/terser.js
+++ tools/terser.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 
-import { minify } from 'terser';
+import { minify } from 'terser'
 
 const config = {
 	compress: {
-		dead_code: true,
-		drop_console: true,
-		drop_debugger: true,
-		keep_classnames: false,
-		keep_fargs: false,
-		keep_fnames: false,
-		keep_infinity: false,
+		'dead_code': true,
+		'drop_console': true,
+		'drop_debugger': true,
+		'keep_classnames': false,
+		'keep_fargs': false,
+		'keep_fnames': false,
+		'keep_infinity': false,
 	},
 	mangle: {
 		eval: false,
-		keep_classnames: false,
-		keep_fnames: false,
+		'keep_classnames': false,
+		'keep_fnames': false,
 		toplevel: true,
 		safari10: false,
 		reserved: ['Session', 'run'], //TODO: Remove 'run' in production
@@ -33,5 +33,5 @@ let data = ''
 stdin.on('data', chunk => data += chunk)
 await new Promise(resolve => stdin.on('end', resolve))
 
-const minified = await minify(data, config);
+const minified = await minify(data, config)
 process.stdout.write(minified.code)
